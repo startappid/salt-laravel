5f1a16586cc262e52f4fa275ad6d64c2a679259e
Farid Hidayat <e.faridhidayat@gmail.com>
2020-07-05 17:43:16 +0700

    generic route web and API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,49 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('api')->prefix('v1')->group(function () {
+
+    Route::group([
+        'prefix' => 'user'
+    ], function () {
+        Route::post('login', 'AuthController@login');
+        Route::post('register', 'AuthController@signup');
+        Route::post('forgot', 'Auth\ForgotPasswordController')->name('forgot.password');
+        Route::patch('password/reset', 'AuthController@resetPassword');
+        Route::group([
+            'middleware' => 'auth:api'
+        ], function() {
+            Route::delete('logout', 'AuthController@logout');
+            Route::get('profile', 'AuthController@profile');
+            Route::get('checkin', 'AuthController@checkin');
+            Route::patch('password', 'AuthController@changePassword');
+            Route::post('fcm', 'AuthController@createUpdateFCM');
+        });
+    });
+
+    // DEFAULT: API RESOURCES
+    Route::get("{collection}", 'Api\ApiResourcesController@index'); // get entire collection
+    Route::post("{collection}", 'Api\ApiResourcesController@store'); // create new collection
+
+    Route::get("{collection}/trash", 'Api\ApiResourcesController@trash'); // trash of collection
+
+    Route::post("{collection}/import", 'Api\ApiResourcesController@import'); // import collection from external
+    Route::post("{collection}/export", 'Api\ApiResourcesController@export'); // export entire collection
+    Route::get("{collection}/report", 'Api\ApiResourcesController@report'); // report collection
+
+    Route::get("{collection}/{id}/trashed", 'Api\ApiResourcesController@trashed')->where('id', '[a-zA-Z0-9]+'); // get collection by ID from trash
+
+    // RESTORE data by ID (id), selected IDs (selected), and All data (all)
+    Route::post("{collection}/{id}/restore", 'Api\ApiResourcesController@restore')->where('id', '[a-zA-Z0-9]+'); // restore collection by ID
+
+    // DELETE data by ID (id), selected IDs (selected), and All data (all)
+    Route::delete("{collection}/{id}/delete", 'Api\ApiResourcesController@delete')->where('id', '[a-zA-Z0-9]+'); // hard delete collection by ID
+
+    Route::get("{collection}/{id}", 'Api\ApiResourcesController@show')->where('id', '[a-zA-Z0-9]+'); // get collection by ID
+    Route::put("{collection}/{id}", 'Api\ApiResourcesController@update')->where('id', '[a-zA-Z0-9]+'); // update collection by ID
+    Route::patch("{collection}/{id}", 'Api\ApiResourcesController@patch')->where('id', '[a-zA-Z0-9]+'); // patch collection by ID
+    // DESTROY data by ID (id), selected IDs (selected), and All data (all)
+    Route::delete("{collection}/{id}", 'Api\ApiResourcesController@destroy')->where('id', '[a-zA-Z0-9]+'); // soft delete a collection by ID
+
+});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+Route::get('{any}', function () {
+    return view('app');
+})->where('any','.*');
