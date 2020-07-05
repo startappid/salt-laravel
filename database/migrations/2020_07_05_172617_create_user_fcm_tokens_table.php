149ebe075974c6f0932b3f212c9521d76e71a8c0
Farid Hidayat <e.faridhidayat@gmail.com>
2020-07-05 17:55:49 +0700

    User FCM migration

---
+++ database/migrations/2020_07_05_172617_create_user_fcm_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserFcmTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_fcm_tokens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('token', 255);
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_fcm_tokens');
+    }
+}
