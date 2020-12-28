b84f443767fa6ac15f5043faf04fc309b1106521
Farid Hidayat <e.faridhidayat@gmail.com>
2020-12-28 20:57:44 +0700

    spatie-laravel and passport

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username', 100)->unique(100);
-            $table->string('email', 200)->unique(200);
+            $table->string('username', 100)->unique();
+            $table->string('email', 100)->unique();
             $table->string('first_name', 50);
             $table->string('last_name', 50);
             $table->timestamp('email_verified_at')->nullable();
