e22ef18b388c5a814feca0454fd9843323c2c48c
Farid Hidayat <e.faridhidayat@gmail.com>
2021-03-09 15:40:02 +0700

    trait grammatical

---
+++ app/Http/Controllers/Grammatical/GrammaticalQuery.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Grammatical;
+
+// NOTE: get this things DONE
+// FIXME: create Laravel package for this concern
+// https://stackoverflow.com/questions/55557713/custom-filtering-on-eloquent-model
+
+trait GrammaticalQuery {
+
+}
