04bac682ba12b0d1133f8024f49af642b55afcc7
Farid Hidayat <e.faridhidayat@gmail.com>
2021-06-09 01:37:51 +0700

    Address model

--- app/Models/Addresses.php
+++ app/Models/Addresses.php
@@ -22,6 +22,26 @@ class Addresses extends Resources {
         'longitude' => 'nullable|float',
     );
 
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'relationship',
+        'withtrashed',
+        'orderby',
+        // Fields table provinces
+        'id',
+        'country_id',
+        'province_id',
+        'city_id',
+        'address',
+        'postalcode',
+        'latitude',
+        'longitude',
+        'foreign_table',
+        'foreign_id'
+    ];
+
     protected $auths = array (
         // 'index',
         'store',
@@ -36,6 +56,63 @@ class Addresses extends Resources {
         'import',
         'export',
         'report'
+    );
+
+    protected $forms = array(
+        [
+            [
+                'class' => 'col-4',
+                'field' => 'country_id'
+            ],
+            [
+                'class' => 'col-4',
+                'field' => 'province_id'
+            ],
+            [
+                'class' => 'col-4',
+                'field' => 'city_id'
+            ]
+        ],
+        [
+            [
+                'class' => 'col',
+                'field' => 'address'
+            ]
+        ],
+        [
+            [
+                'class' => 'col-2',
+                'field' => 'postalcode'
+            ],
+            [
+                'class' => 'col-3',
+                'field' => 'latitude'
+            ],
+            [
+                'class' => 'col-3',
+                'field' => 'longitude'
+            ]
+        ],
+        [
+            [
+                'class' => 'col-2',
+                'field' => 'type'
+            ],
+            [
+                'class' => 'col-4',
+                'field' => 'category'
+            ]
+        ],
+        [
+            [
+                'class' => 'col-2',
+                'field' => 'foreign_table'
+            ],
+            [
+                'class' => 'col-2',
+                'field' => 'foreign_id'
+            ],
+        ]
     );
 
     protected $structures = array(
@@ -56,14 +133,14 @@ class Addresses extends Resources {
         ],
         "foreign_table" => [
             'name' => 'foreign_table',
-            'label' => 'Table name',
-            'display' => false,
+            'label' => 'Foreign Table',
+            'display' => true,
             'validation' => [
                 'create' => 'nullable|string',
                 'update' => 'nullable|string',
                 'delete' => null,
             ],
-            'required' => true,
+            'required' => false,
             'primary' => false,
             'type' => 'text',
             'validated' => true,
@@ -73,14 +150,14 @@ class Addresses extends Resources {
         ],
         "foreign_id" => [
             'name' => 'foreign_id',
-            'label' => 'Table field',
-            'display' => false,
+            'label' => 'Foreign ID',
+            'display' => true,
             'validation' => [
                 'create' => 'nullable|integer',
                 'update' => 'nullable|integer',
                 'delete' => null,
             ],
-            'required' => true,
+            'required' => false,
             'primary' => false,
             'type' => 'text',
             'validated' => true,
@@ -91,108 +168,190 @@ class Addresses extends Resources {
         "country_id" => [
             'name' => 'country_id',
             'label' => 'Country',
-            'display' => false,
+            'display' => true,
             'validation' => [
                 'create' => 'required|integer',
                 'update' => 'required|integer',
                 'delete' => null,
             ],
+            'required' => true,
             'primary' => false,
-            'type' => 'text',
+            'type' => 'reference',
             'validated' => true,
             'nullable' => false,
-            'note' => null
+            'note' => null,
+            'placeholder' => null,// Options reference
+            'reference' => "countries", // Select2 API endpoint => /api/v1/countries
+            'relationship' => 'country', // relationship request datatable
+            'option' => [
+                'value' => 'id',
+                'label' => 'name'
+            ]
         ],
         "province_id" => [
             'name' => 'province_id',
             'label' => 'Province',
-            'display' => false,
+            'display' => true,
             'validation' => [
                 'create' => 'required|integer',
                 'update' => 'required|integer',
                 'delete' => null,
             ],
+            'required' => true,
             'primary' => false,
-            'type' => 'text',
+            'type' => 'reference',
             'validated' => true,
             'nullable' => false,
-            'note' => null
+            'note' => null,
+            'placeholder' => null,// Options reference
+            'reference' => "provinces", // Select2 API endpoint => /api/v1/countries
+            'relationship' => 'province', // relationship request datatable
+            'option' => [
+                'value' => 'id',
+                'label' => 'name'
+            ]
         ],
         "city_id" => [
             'name' => 'city_id',
             'label' => 'City',
-            'display' => false,
+            'display' => true,
             'validation' => [
                 'create' => 'required|integer',
                 'update' => 'required|integer',
                 'delete' => null,
             ],
+            'required' => true,
             'primary' => false,
-            'type' => 'text',
+            'type' => 'reference',
             'validated' => true,
             'nullable' => false,
-            'note' => null
+            'note' => null,
+            'placeholder' => null,// Options reference
+            'reference' => "cities", // Select2 API endpoint => /api/v1/countries
+            'relationship' => 'city', // relationship request datatable
+            'option' => [
+                'value' => 'id',
+                'label' => 'name'
+            ]
         ],
         "address" => [
             'name' => 'address',
             'label' => 'Address',
-            'display' => false,
+            'display' => true,
             'validation' => [
                 'create' => 'required|string|max:255',
                 'update' => 'required|string|max:255',
                 'delete' => null,
             ],
+            'required' => true,
             'primary' => false,
             'type' => 'text',
             'validated' => true,
             'nullable' => false,
-            'note' => null
+            'note' => null,
+            'placeholder' => null,
         ],
         "postalcode" => [
             'name' => 'postalcode',
             'label' => 'Postal Code',
-            'display' => false,
+            'display' => true,
             'validation' => [
                 'create' => 'required|string|max:5',
                 'update' => 'required|string|max:5',
                 'delete' => null,
             ],
+            'required' => true,
             'primary' => false,
             'type' => 'text',
             'validated' => true,
             'nullable' => false,
-            'note' => null
+            'note' => null,
+            'placeholder' => null,
         ],
         "latitude" => [
             'name' => 'latitude',
             'label' => 'Latitude',
-            'display' => false,
+            'display' => true,
             'validation' => [
                 'create' => 'nullable|float',
                 'update' => 'nullable|float',
                 'delete' => null,
             ],
+            'required' => false,
             'primary' => false,
             'type' => 'text',
             'validated' => true,
             'nullable' => false,
-            'note' => null
+            'note' => null,
+            'placeholder' => null,
         ],
         "longitude" => [
             'name' => 'longitude',
             'label' => 'Longitude',
-            'display' => false,
+            'display' => true,
             'validation' => [
                 'create' => 'nullable|float',
                 'update' => 'nullable|float',
                 'delete' => null,
             ],
+            'required' => false,
             'primary' => false,
             'type' => 'text',
             'validated' => true,
             'nullable' => false,
-            'note' => null
+            'note' => null,
+            'placeholder' => null,
         ],
+        "type" => [
+            'name' => 'type',
+            'default' => 'other',
+            'label' => 'Type',
+            'display' => true,
+            'validation' => [
+                'create' => 'required|string',
+                'update' => 'required|string',
+                'delete' => null,
+            ],
+            'primary' => false,
+            'required' => true,
+            'type' => 'radio',
+            'validated' => true,
+            'nullable' => false,
+            'note' => '— Address Type',
+            'placeholder' => null,
+            // Options checkbox
+            'inline' => true,
+            'options' => [
+                [
+                    'value' => 'primary',
+                    'label' => 'Primary'
+                ],
+                [
+                    'value' => 'other',
+                    'label' => 'Other'
+                ],
+            ],
+            // Options disabled according to value
+            'options_disabled' => []
+        ],
+        "category" => [
+            'name' => 'category',
+            'label' => 'Category',
+            'display' => true,
+            'validation' => [
+                'create' => 'nullable|string',
+                'update' => 'nullable|string',
+                'delete' => null,
+            ],
+            'required' => false,
+            'primary' => false,
+            'type' => 'text',
+            'validated' => true,
+            'nullable' => false,
+            'note' => null,
+            'placeholder' => null,
+        ],
+
         "created_at" => [
             'name' => 'created_at',
             'label' => 'Created At',
