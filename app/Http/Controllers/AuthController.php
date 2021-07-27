667230047c743dfaf9c472335fe587a6067a0575
Farid Hidayat <e.faridhidayat@gmail.com>
2021-07-27 22:28:23 +0700

    some features in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,13 +5,18 @@ use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Users;
+use App\Models\Addresses;
+use App\Models\Files;
 use App\Models\UserFcmTokens;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 use App\Services\ResponseService;
 use Spatie\Permission\Exceptions\UnauthorizedException;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Events\UserCheck;
+use Illuminate\Support\Arr;
 use DB;
 use Curl;
 
@@ -66,6 +71,10 @@ class AuthController extends Controller {
                 $model->setAttribute($key, $value);
             }
             $model->save();
+
+            $user = User::find($model->id);
+            $user->sendEmailVerificationNotification();
+
             $this->responder->set('message', Str::title('You\'re registered!'));
             $this->responder->set('data', $model);
             $this->responder->setStatus(201, 'Created.');
@@ -179,6 +188,118 @@ class AuthController extends Controller {
         return $this->responder->response();
     }
 
+    /**
+     * Create user
+     *
+     * @param  [string] name
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [string] password_confirmation
+     * @return [string] message
+     */
+    public function updateProfile(Request $request) {
+
+        try {
+
+            $rules = [
+                "email" => "required|email",
+                "first_name" => "required|string",
+                "last_name" => "required|string",
+                "dob" => "required|date",
+                "gender" => "required|string",
+                "phone" => "required|string",
+                "address" => "nullable|array"
+            ];
+
+            $validator = Validator::make($request->all(), $rules, $this->messages);
+            if ($validator->fails()) {
+                $this->responder->set('errors', $validator->errors());
+                $this->responder->setStatus(400, 'Bad Request');
+                $this->responder->set('message', $validator->errors()->first());
+                return $this->responder->response();
+            }
+
+            $user = Auth::user();
+            $fields = $request->only([
+                "email",
+                "first_name",
+                "last_name",
+                "dob",
+                "gender",
+                "phone"
+            ]);
+            foreach ($fields as $key => $value) {
+                $user->setAttribute($key, $value);
+            }
+            $user->save();
+
+            if($request->has('address')) {
+                $address = Addresses::where('foreign_id', $user->id)
+                                    ->where('foreign_table', 'users')
+                                    ->first();
+
+                if(is_null($address)) {
+                    $address = new Addresses();
+                }
+
+                $data = array_merge($request->get('address'), [
+                    'foreign_id' => $user->id,
+                    'foreign_table' => 'users'
+                ]);
+
+                foreach ($data as $key => $value) {
+                    $address->setAttribute($key, $value);
+                }
+                $address->save();
+            }
+
+            $this->responder->setStatus(200, 'Ok');
+            $this->responder->set('message', 'Profile updated');
+            $this->responder->set('data', $user);
+            return $this->responder->response();
+
+        } catch (\Exception $e) {
+            $this->responder->set('message', $e->getMessage());
+            $this->responder->setStatus(500, 'Internal server error.');
+            return $this->responder->response();
+        }
+    }
+    /**
+     * Get the authenticated User
+     *
+     * @return [json] user object
+     */
+    public function photo(Request $request)
+    {
+        $user = Auth::user();
+        $profile = Files::where('foreign_table', 'users')
+                        ->where('foreign_id', $user->id)
+                        ->where('directory', 'users/profile')
+                        ->first();
+
+        $this->responder->set('collection', 'User');
+        $this->responder->set('message', 'Data retrieved');
+        $this->responder->set('data', $profile);
+        return $this->responder->response();
+    }
+
+    /**
+     * Get the authenticated User
+     *
+     * @return [json] user object
+     */
+    public function permissions(Request $request)
+    {
+        $user = Auth::user();
+        $permissions = $user->getAllPermissions();
+        $permissions = Arr::pluck($permissions, 'name');
+
+        $this->responder->set('collection', 'Permissions');
+        $this->responder->set('message', 'Data retrieved');
+        $this->responder->set('data', $permissions);
+        return $this->responder->response();
+    }
+
 
     /**
      * Create user
@@ -262,10 +383,6 @@ class AuthController extends Controller {
                 $this->responder->set('message', $validator->errors()->first());
                 return $this->responder->response();
             }
-
-            $request->validate([
-                'email' => 'required|email',
-            ]);
 
             // We will send the password reset link to this user. Once we have attempted
             // to send the link, we will examine the response then see the message we
