8517f3f6d49484034c5354d71449a120afe99518
Farid Hidayat <e.faridhidayat@gmail.com>
2021-07-29 01:03:35 +0700

    validation to rules attribute

--- app/Models/Banners.php
+++ app/Models/Banners.php
@@ -35,7 +35,14 @@ class Banners extends Resources {
         'order'
     ];
 
-    protected $rules = array();
+    protected $rules = array(
+        "title" => 'required|string',
+        "description" => 'required|string',
+        "status" => 'nullable|string',
+        "type" => 'nullable|string',
+        "videourl" => 'nullable|string',
+        "order" => 'required|integer',
+    );
 
     protected $auths = array (
         // 'index',
@@ -53,185 +60,7 @@ class Banners extends Resources {
         'report'
     );
 
-    protected $structures = array(
-        "id" => [
-            'name' => 'id',
-            'default' => null,
-            'label' => 'ID',
-            'display' => false,
-            'validation' => [
-                'create' => null,
-                'update' => null,
-                'delete' => null,
-            ],
-            'primary' => true,
-            'required' => true,
-            'type' => 'integer',
-            'validated' => false,
-            'nullable' => false,
-            'note' => null
-        ],
-        "title" => [
-            'name' => 'title',
-            'default' => null,
-            'label' => 'Title',
-            'display' => true,
-            'validation' => [
-                'create' => 'required|string',
-                'update' => 'required|string',
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => true,
-            'type' => 'text',
-            'validated' => true,
-            'nullable' => false,
-            'note' => null,
-            'placeholder' => null,
-        ],
-        "description" => [
-            'name' => 'description',
-            'default' => null,
-            'label' => 'Description',
-            'display' => true,
-            'validation' => [
-                'create' => 'required|string',
-                'update' => 'required|string',
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => true,
-            'type' => 'text',
-            'validated' => true,
-            'nullable' => false,
-            'note' => null,
-            'placeholder' => null,
-        ],
-        "status" => [
-            'name' => 'status',
-            'default' => null,
-            'label' => 'Status',
-            'display' => true,
-            'validation' => [
-                'create' => 'nullable|string',
-                'update' => 'nullable|string',
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => true,
-            'type' => 'text',
-            'validated' => true,
-            'nullable' => false,
-            'note' => null,
-            'placeholder' => null,
-        ],
-        "type" => [
-            'name' => 'type',
-            'default' => null,
-            'label' => 'Type',
-            'display' => true,
-            'validation' => [
-                'create' => 'nullable|string',
-                'update' => 'nullable|string',
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => true,
-            'type' => 'text',
-            'validated' => true,
-            'nullable' => false,
-            'note' => null,
-            'placeholder' => null,
-        ],
-        "videourl" => [
-            'name' => 'videourl',
-            'default' => null,
-            'label' => 'Video URL',
-            'display' => true,
-            'validation' => [
-                'create' => 'nullable|string',
-                'update' => 'nullable|string',
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => true,
-            'type' => 'text',
-            'validated' => true,
-            'nullable' => false,
-            'note' => null,
-            'placeholder' => null,
-        ],
-        "order" => [
-            'name' => 'order',
-            'default' => null,
-            'label' => 'Order',
-            'display' => true,
-            'validation' => [
-                'create' => 'required|integer',
-                'update' => 'required|integer',
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => true,
-            'type' => 'text',
-            'validated' => true,
-            'nullable' => false,
-            'note' => null,
-            'placeholder' => null,
-        ],
-
-        "created_at" => [
-            'name' => 'created_at',
-            'default' => null,
-            'label' => 'Created At',
-            'display' => false,
-            'validation' => [
-                'create' => null,
-                'update' => null,
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => false,
-            'type' => 'datetime',
-            'validated' => false,
-            'nullable' => false,
-            'note' => null
-        ],
-        "updated_at" => [
-            'name' => 'updated_at',
-            'default' => null,
-            'label' => 'Updated At',
-            'display' => false,
-            'validation' => [
-                'create' => null,
-                'update' => null,
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => false,
-            'type' => 'datetime',
-            'validated' => false,
-            'nullable' => false,
-            'note' => null
-        ],
-        "deleted_at" => [
-            'name' => 'deleted_at',
-            'default' => null,
-            'label' => 'Deleted At',
-            'display' => false,
-            'validation' => [
-                'create' => null,
-                'update' => null,
-                'delete' => null,
-            ],
-            'primary' => false,
-            'required' => false,
-            'type' => 'datetime',
-            'validated' => false,
-            'nullable' => false,
-            'note' => null
-        ]
-    );
+    protected $structures = array();
 
     protected $forms = array();
     protected $searchable = array( 'title', 'description', 'status', 'type','videourl','order', 'link');
