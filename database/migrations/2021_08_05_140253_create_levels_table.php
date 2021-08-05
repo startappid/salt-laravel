e7c81b6006e46e85dde7fcfc24dfb4043b8e9375
Farid Hidayat <e.faridhidayat@gmail.com>
2021-08-05 21:40:23 +0700

    levels migrations table

---
+++ database/migrations/2021_08_05_140253_create_levels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLevelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('levels', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('degree_id')->constrained('degrees');
+            $table->tinyInteger('level')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('levels');
+    }
+}
