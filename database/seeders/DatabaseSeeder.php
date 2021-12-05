0c9407e775546746571c1ace79329b6014bf08e3
Farid Hidayat <e.faridhidayat@gmail.com>
2021-12-05 19:37:22 +0700

    remove countries migration and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             RolesAndPermissionsSeeder::class,
-            SqlCountriesSeeder::class,
         ]);
     }
 }
