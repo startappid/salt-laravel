f6977e12a47480ffb90a3ff2faafa83b76fdace2
Farid Hidayat <farid.hidayat@sagara.id>
2022-06-13 10:55:34 +0700

    chore: not finished yet

--- database/migrations/2020_07_05_172617_create_user_fcm_tokens_table.php
+++ database/migrations/2020_07_05_172617_create_user_fcm_tokens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserFcmTokensTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateUserFcmTokensTable extends Migration
     {
         Schema::dropIfExists('user_fcm_tokens');
     }
-}
+};
--- database/migrations/2021_07_25_155435_create_chat_session_users_table.php
+++ database/migrations/2021_07_25_155435_create_chat_session_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChatSessionUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateChatSessionUsersTable extends Migration
     {
         Schema::dropIfExists('chat_session_users');
     }
-}
+};
