794d4d308ced85e15db48e2c911d6f6bc659cf2b
Farid Hidayat <e.faridhidayat@gmail.com>
2022-10-04 22:32:59 +0700

    fix: use traits from SaltLaravel

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 use DB;
 use Illuminate\Support\Facades\Schema;
 use App\Observers\UsersObserver as Observer;
-use App\Traits\ObservableModel;
-use App\Traits\Uuids;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
 
 class Users extends Resources {
 
